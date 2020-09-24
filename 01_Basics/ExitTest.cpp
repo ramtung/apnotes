ac6cac4dcda130c07bc54ef4bb70d7471be0ebd2
ramtung <ramtung@gmail.com>
2020-09-24 09:41:46 +0330

    Updated 01_Basics and 02_Vectors

--- 01_Basics/ExitTest.cpp
+++ 01_Basics/ExitTest.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main()
 {
 	string name;
-	cout << "Please enter your name:";
+	cout << "Please enter your name: ";
 	cin >> name;
-	if (name == "Ramtin") {
-		cout << "NAAAAH! Ramtin?!\n";
+	if (name == "Toby") {
+		cerr << "You're a waste of life, and you should give up.\n";
 		return 1;
 	}
 	cout << "Hello " << name << "\n";
--- 02_Vectors/02_VecAllocInit.cpp
+++ 02_Vectors/02_VecAllocInit.cpp
@@ -10,13 +10,32 @@ int main()
 	vec.push_back(1.3);				
 	vec[0] = 12.4;
 
+
+
+
+
+
+
+
+
 	vector<int> v(6);
 
-	v[0] = 5; v[1] = 7;
-	v[2] = 9; v[3] = 4;
-	v[4] = 6; v[5] = 8;
+	v[0] = 5; 
+	v[1] = 7;
+	v[2] = 9; 
+	v[3] = 4;
+	v[4] = 6; 
+	v[5] = 8;
+
+
+
+
+
+
+
 
 	vector<string> philosopher(4);
+
 	philosopher [0] = "Kant"; 
 	philosopher [1] = "Plato";
 	philosopher [2] = "Hume";
@@ -24,27 +43,55 @@ int main()
 
 	//philosopher[2] = 99;
 	
-	vector<double> vd(1000, 1.2);
+	
+	
+	
+	
+	
+	
 
-	vector<vector<int>> twod(5);
+	vector<double> vd(1000, 1.2);
+	//vd[1000] = 4.7;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	vector<vector<int> > twod(5);
+		
 	twod[0].push_back(12);
 	twod[0].push_back(45);
 	twod[1].push_back(3);
 	twod[2].push_back(1);
 	twod[2].push_back(6);
 	
-	for (int i = 0; i < twod.size(); i++) {
-		cout << '[';
-		for (int j = 0; j < twod[i].size(); j++)
-			cout << twod[i][j] << ' ';
-		cout << ']' << endl;
-	}
-
-	// Since C++11
-	vector<int> list = {1, 6, 3};
-
-	// Since C++11
-	for (int i : list) {
-		cout << i << ' ';
-	}
+	/*
+		[12, 45]
+		[3]
+		[1, 6]
+		[]
+		[]
+	
+	*/
 }
+
+
+
+
+
+
+
+
+
