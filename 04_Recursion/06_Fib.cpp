e3b5da008d632ffc820badb85514a238a7ae2c42
ramtung <ramtung@gmail.com>
2020-09-27 09:33:00 +0330

    Added Fib(0) Case

--- 04_Recursion/06_Fib.cpp
+++ 04_Recursion/06_Fib.cpp
@@ -4,31 +4,52 @@ using namespace std;
 
 long fib(long n)
 {
-    if (n <= 2)
-        return 1;
+    if (n <= 1)
+        return n;
     return fib(n - 1) + fib (n - 2);
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 long nr_fib(long n)
 {
+    if (n <= 0)
+        return 0;
+
     long a = 1;
     long b = 1;
-    long c;
     
     for (long i = 0; i < n - 2; i++) {
-        c = a + b;
+        long c = a + b;
         a = b;
         b = c;
     }
-    return c;
+    return b;
 }
+
+
+
+
+
+
 
 int main()
 {
     clock_t t;
-    for (int n = 1; n < 50; n++) {
+    for (int n = 0; n < 50; n++) {
         cout << "n=" << n << '\t';
-        
+
         t = clock();
         long f = nr_fib(n);
         t = clock() - t;
