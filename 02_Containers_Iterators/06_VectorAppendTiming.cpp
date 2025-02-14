ec14f6ed5afb3dfd68ff186c47081a5762aa4ef3
ramtung <ramtung@gmail.com>
2025-02-14 09:34:54 +0330

    Update 06_VectorAppendTiming.cpp

--- 02_Containers_Iterators/06_VectorAppendTiming.cpp
+++ 02_Containers_Iterators/06_VectorAppendTiming.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <list>
 #include <iostream>
+#include <iomanip>
 #include <chrono>
 
 using namespace std;
@@ -9,24 +10,27 @@ using namespace chrono;
 int main()
 {
 	vector<double> v;
-	for (int i = 0; i < 10000; i++) {
-		cout << i << "\tsize = " << v.size() 
-		          << "\tcapacity = " << v.capacity();
-		
-		steady_clock::time_point begin = steady_clock::now();
-		v.push_back(0);
-		steady_clock::time_point end = steady_clock::now();
-		cout << "\ttime = " << duration_cast<nanoseconds>(end-begin).count() << "[ns]" << endl;
-	}
-
 	list<double> l;
-	for (int i = 0; i < 10000; i++) {
-		cout << i << "\tsize = " << l.size();
-		
-		steady_clock::time_point begin = steady_clock::now();
-		l.push_back(0);
-		steady_clock::time_point end = steady_clock::now();
-		cout << "\ttime = " << duration_cast<nanoseconds>(end-begin).count() << "[ns]" << endl;
-	}
 
+  steady_clock::time_point begin;
+  steady_clock::time_point end;
+
+	for (int i = 0; i < 10000; i++) {
+		cout << "size = " << v.size() 
+      << "\tcapacity = " << setw(5) << v.capacity();
+		
+		begin = steady_clock::now();
+		v.push_back(0);
+		end = steady_clock::now();
+		cout << "\tvector: " << setw(6)
+      << duration_cast<nanoseconds>(end-begin).count() 
+      << "[ns]";
+
+		begin = steady_clock::now();
+		l.push_back(0);
+		end = steady_clock::now();
+		cout << "\tlist:"  << setw(6)
+      << duration_cast<nanoseconds>(end-begin).count() 
+      << "[ns]\n";
+	}
 }
