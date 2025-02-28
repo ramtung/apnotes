8f026c4e514cac70272307c37ec0ab61299ae2bf
ramtung <ramtung@gmail.com>
2025-02-28 21:15:14 +0330

    Fibonacci Cleaned Up

--- 06_Recursion/06_Fib.cpp
+++ 06_Recursion/06_Fib.cpp
@@ -1,63 +1,54 @@
 #include <iostream>
-#include <ctime>
+#include <iomanip>
+#include <chrono>
 using namespace std;
+using namespace chrono;
 
-long fib(long n)
-{
-    if (n <= 1)
-        return n;
-    return fib(n - 1) + fib (n - 2);
+long fib(long n) {
+  if (n <= 1)
+    return n;
+  return fib(n - 1) + fib (n - 2);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-long nr_fib(long n)
-{
-    if (n <= 0)
-        return 0;
-
-    long a = 1;
-    long b = 1;
-    
-    for (long i = 0; i < n - 2; i++) {
-        long c = a + b;
-        a = b;
-        b = c;
-    }
-    return b;
+long nr_fib(long n) {
+  if (n <= 0)
+    return 0;
+  long a = 1;
+  long b = 1;
+  for (long i = 0; i < n - 2; i++) {
+    long c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
 }
 
+void printDuration(steady_clock::time_point start, 
+                   steady_clock::time_point end) {
+  cout 
+    << setw(10) << fixed << setprecision(5) 
+    << duration_cast<nanoseconds>(end - start).count() * 1e-6
+    << " (ms)";
+}
 
+int main() {
+  cout << setw(5) << "n"
+       << setw(15) << "Non-Recursive"
+       << setw(15) << "Recursive"
+       << endl << string(35, '-') << endl;
 
+  for (int n = 0; n < 50; n++) {
+    cout << setw(5) << n;
 
+    auto start = steady_clock::now();
+    nr_fib(n);
+    auto end = steady_clock::now();
+    printDuration(start, end);
 
-
-
-int main()
-{
-    clock_t t;
-    for (int n = 0; n < 50; n++) {
-        cout << "n=" << n << '\t';
-
-        t = clock();
-        long f = nr_fib(n);
-        t = clock() - t;
-        cout << "Non-Rec: " << 1000*t/CLOCKS_PER_SEC << "(ms)\t";
-
-        t = clock();
-        f = fib(n);
-        t = clock() - t;
-        cout << "Rec: " << 1000*t/CLOCKS_PER_SEC << "(ms)\n";
-    }
+    start = steady_clock::now();
+    fib(n);
+    end = steady_clock::now();
+    printDuration(start, end);
+    cout << endl;
+  }
 }
