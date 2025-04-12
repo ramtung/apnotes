f18114d044e685055c403fc798540794aaa5fa3e
Ramtin <ramtung@gmail.com>
2025-04-12 10:36:49 +0330

    Update OOSimulation

--- 09_OOSimulation/01_billiards.cpp
+++ 09_OOSimulation/01_billiards.cpp
@@ -1,83 +1,77 @@
 #include <iostream>
-#include <cstdlib>
+#include <stdexcept>
 using namespace std;
 
 class Table {
 public:
-    Table(double w, double h);
-    bool contains(double x, double y) {
-        return x >= 0 && x <= width && y >= 0 && y < height;
-    }
-    double get_width() { return width; }
-    double get_height() { return height; }
+  Table(double w, double h);
+  double getWidth() { return width; }
+  double getHeight() { return height; }
 private:
-    double width;
-    double height;
+  double width;
+  double height;
 };
 
-Table::Table(double w, double h) 
-{
-    if (w <= 0 || h <= 0)
-        abort();
-    width = w;
-    height = h;
+Table::Table(double w, double h) {
+  if (w <= 0 || h <= 0)
+    throw invalid_argument("Table dimensions must be positive");
+  width = w;
+  height = h;
 }
 
 class Ball {
 public:
-    Ball(double _x, double _y, double _vx, double _vy, Table* t);
-    void move(double dt);
-    double get_x() { return x; }
-    double get_y() { return y; }
-    double get_vx() { return vx; }
-    double get_vy() { return vy; }
+  Ball(double _x, double _y, double _vx, double _vy, Table* t);
+  void move(double dt);
+  double getX() { return x; }
+  double getY() { return y; }
+  double getVx() { return vx; }
+  double getVy() { return vy; }
 private:
-    double x;
-    double y;
-    double vx;
-    double vy;
-    Table* table;
+  double x;
+  double y;
+  double vx;
+  double vy;
+  Table* table;
 };
 
-Ball::Ball(double _x, double _y, double _vx, double _vy, Table* t)
-{
-    table = t;
-    if (!table->contains(_x, _y))
-        abort();
-    x = _x;
-    y = _y;
-    vx = _vx;
-    vy = _vy;
+Ball::Ball(double _x, double _y, double _vx, double _vy, Table* t) {
+  table = t;
+  if (x < 0 || x >= table->getWidth() || y < 0 || y >= table->getHeight())
+    throw invalid_argument("Ball position out of bounds");
+  x = _x;
+  y = _y;
+  vx = _vx;
+  vy = _vy;
 }
 
-void Ball::move(double dt)
-{
-    x += vx * dt;
-    y += vy * dt;
-    
-    while (!table->contains(x, y)) {
-        if (x < 0) {
-            x = -x;
-            vx = -vx;
-        }
-        if (x >= table->get_width()) {
-            x = 2 * table->get_width() - x;
-            vx = -vx;
-        }
-        if (y < 0) {
-            y = -y;
-            vy = -vy;
-        }
-        if (y >= table->get_height()) {
-            y = 2 * table->get_height() - y;
-            vy = -vy;
-        }
+void Ball::move(double dt) {
+  x += vx * dt;
+  y += vy * dt;
+  
+  while (x < 0 || x >= table->getWidth() || y < 0 || y >= table->getHeight()) {
+    if (x < 0) {
+      x = -x;
+      vx = -vx;
     }
+    if (x >= table->getWidth()) {
+      x = 2 * table->getWidth() - x;
+      vx = -vx;
+    }
+    if (y < 0) {
+      y = -y;
+      vy = -vy;
+    }
+    if (y >= table->getHeight()) {
+      y = 2 * table->getHeight() - y;
+      vy = -vy;
+    }
+  }
 }
 
 int main()
 {
-    Table t(100, 50);
-    Ball b(10, 20, 25, 5, &t);
-    b.move(10); 
+  Table t(100, 50);
+  Ball b(10, 20, 25, 5, &t);
+  b.move(10); 
 }
--- 09_OOSimulation/02_billiards.cpp
+++ 09_OOSimulation/02_billiards.cpp
@@ -1,98 +1,83 @@
 #include <iostream>
-#include <cstdlib>
+#include <stdexcept>
 using namespace std;
-
-class Point {
-    double x;
-    double y;
-};
-
-class Vector {
-    double x;
-    double y;
-}
-
-class MotionState {
-    Point location;
-    Vector velocity;
-};
 
 class Table {
 public:
-    Table(double w, double h);
-    bool contains_point(Point p);
-    MotionState reflect(MotionState mo);
+  Table(double w, double h);
+  bool containsPoint(double x, double y);
+  void reflect(double& x, double& y, double& vx, double& vy);
 private:
-    double width;
-    double height;
+  double width;
+  double height;
 };
 
-Table::Table(double w, double h) 
-{
-    if (w <= 0 || h <= 0)
-        abort();
-    width = w;
-    height = h;
+Table::Table(double w, double h) {
+  if (w <= 0 || h <= 0)
+    throw invalid_argument("Table dimensions must be positive");
+  width = w;
+  height = h;
 }
 
-bool Table::contains_point(Point p) {
-    return p.x >= 0 && p.x < width && p.y >= 0 && p.y < height;
+bool Table::containsPoint(double x, double y) {
+  return x >= 0 && x < width && y >= 0 && y < height;
 }
 
 void Table::reflect(double& x, double& y, double& vx, double& vy) {
-    if (x < 0) {
-        x = -x;
-        vx = -vx;
-    }
-    if (x >= width) {
-        x = 2 * width - x;
-        vx = -vx;
-    }
-    if (y < 0) {
-        y = -y;
-        vy = -vy;
-    }
-    if (y >= height) {
-        y = 2 * height - y;
-        vy = -vy;
-    }
+  if (x < 0) {
+    x = -x;
+    vx = -vx;
+  }
+  if (x >= width) {
+    x = 2 * width - x;
+    vx = -vx;
+  }
+  if (y < 0) {
+    y = -y;
+    vy = -vy;
+  }
+  if (y >= height) {
+    y = 2 * height - y;
+    vy = -vy;
+  }
 }
 
 class Ball {
 public:
-    Ball(double _x, double _y, double _vx, double _vy, Table* t);
-    void move(double dt);
-    double get_x() { return x; }
-    double get_y() { return y; }
-    double get_vx() { return vx; }
-    double get_vy() { return vy; }
+  Ball(double _x, double _y, double _vx, double _vy, Table* t);
+  void move(double dt);
+  double getX() { return x; }
+  double getY() { return y; }
+  double getVx() { return vx; }
+  double getVy() { return vy; }
 private:
-    MotionState mo;
-    Table* table;
+private:
+  double x;
+  double y;
+  double vx;
+  double vy;
+  Table* table;
 };
 
-Ball::Ball(double _x, double _y, double _vx, double _vy, Table* t)
-{
-    table = t;
-    if (!table->contains_point(_x, _y))
-        abort();
-    x = _x;
-    y = _y;
-    vx = _vx;
-    vy = _vy;
+Ball::Ball(double _x, double _y, double _vx, double _vy, Table* t) {
+  table = t;
+  if (!table->containsPoint(_x, _y))
+    throw invalid_argument("Ball position out of bounds");
+  x = _x;
+  y = _y;
+  vx = _vx;
+  vy = _vy;
 }
 
-void Ball::move(double dt)
-{
-    location.translate(vx * dt, vy * dt);
-    
-    while (!table->contains_point(location))
-        mo = table->reflect(mo);
+void Ball::move(double dt) {
+  x += vx * dt;
+  y += vy * dt;  
+  while (!table->containsPoint(x, y))
+    table->reflect(x, y, vx, vy);
 }
 
-int main()
-{
-    Table t(100, 50);
-    Ball b(10, 20, 25, 5, &t);
-    b.move(10); 
+int main() {
+  Table t(100, 50);
+  Ball b(10, 20, 25, 5, &t);
+  b.move(10); 
 }
